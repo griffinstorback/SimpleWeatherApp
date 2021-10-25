d5d4fc943ce8cb5172c1e69c8285eeb522a41e1a
Griffin <griffinstorback@gmail.com>
2021-10-25 16:12:53 -0700

    some stuff, viewmodels and whatnot

--- SimpleWeatherApp/UI/ViewControllers/AddLocationViewController.swift
+++ SimpleWeatherApp/UI/ViewControllers/AddLocationViewController.swift
@@ -9,26 +9,54 @@ import UIKit
 
 /// "Add location" modal, presented from "+" in LocationsTableView rightBarButtonItem
 class AddLocationViewController: UIViewController {
-
+    let viewModel: AddLocationViewModel
+    
+    private let searchController: UISearchController
+    private let resultsTableView: UITableViewController
+    private let resultsTableViewDataSource: UITableViewDiffableDataSource<Int, Int>
+    
+    init(viewModel: AddLocationViewModel = AddLocationViewModel()) {
+        self.viewModel = viewModel
+        
+        resultsTableView = UITableViewController(style: .plain)
+        
+        // set up resultsTableView DataSource
+        resultsTableViewDataSource = UITableViewDiffableDataSource<Int, Int>(tableView: resultsTableView.tableView, cellProvider: { tableView, indexPath, section -> UITableViewCell? in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LocationResultCell", for: indexPath) as UITableViewCell
+            
+            return UITableViewCell()
+        })
+        
+        searchController = UISearchController(searchResultsController: resultsTableView)
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Add location"
-        view.backgroundColor = .systemTeal
+        view.backgroundColor = .systemBackground
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneButtonPressed))
+        navigationItem.searchController = searchController
         
-        NetworkManager.shared.getLocationsForSearchText("san") { result in
-            switch result {
-            case .failure(let error):
-                print("error getting locations in AddLocationVC:", error)
-            case .success(let locations):
-                print(locations)
-            }
-        }
+        searchController.searchResultsUpdater = self
+        resultsTableView
     }
     
     @objc func doneButtonPressed() {
         self.dismiss(animated: true)
+    }
+}
+
+extension AddLocationViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let changedSearchText = searchController.searchBar.text else { return }
+        viewModel.searchText.send(changedSearchText)
     }
 }
--- SimpleWeatherApp/ViewModels/AddLocationViewModel.swift
+++ SimpleWeatherApp/ViewModels/AddLocationViewModel.swift
@@ -6,10 +6,43 @@
 //
 
 import Foundation
+import Combine
 
-class AddLocationViewModel: ObservableObject {
-    /// Shows queried locations (queried using searchText)
-    @Published var displayedLocations: [Location] = []
+protocol AddLocationViewModelProtocol {
+    var displayedLocations: CurrentValueSubject<[Location], Never> { get }
+    var searchText: CurrentValueSubject<String, Never> { get }
+}
+
+class AddLocationViewModel: AddLocationViewModelProtocol, ObservableObject {
+    private let networkManager: NetworkManagerProtocol
     
-    @Published var searchText: String = ""
+    var displayedLocations = CurrentValueSubject<[Location], Never>([])
+    var searchText = CurrentValueSubject<String, Never>("")
+    var cancellables = [AnyCancellable]()
+    
+    init(networkManager: NetworkManagerProtocol = NetworkManager.shared) {
+        self.networkManager = networkManager
+        
+        // when search text changed, update search results
+        searchText.sink { [weak self] updatedText in
+            self?.updateDisplayedLocations(text: updatedText)
+        }.store(in: &cancellables)
+    }
+    
+    private func updateDisplayedLocations(text: String) {
+        // if text empty, no results (obvs)
+        guard !text.isEmpty else {
+            displayedLocations.send([])
+            return
+        }
+        
+        NetworkManager.shared.getLocationsForSearchText(text) { [weak self] result in
+            switch result {
+            case .failure(let error):
+                print("error getting locations in AddLocationVC:", error)
+            case .success(let locations):
+                self?.displayedLocations.send(locations)
+            }
+        }
+    }
 }
